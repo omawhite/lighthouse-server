8a8ab5d72853f8f8191c26e159b0b5dcd1735bcc
Omar White <omawhite@indiana.edu>
2024-03-09 15:40:25 -0500

    wip

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const {createServer} = require('@lhci/server');
 
 console.log('Starting server...');
@@ -5,7 +6,8 @@ createServer({
   port: process.env.PORT,
   storage: {
     storageMethod: 'sql',
-    sqlDialect: 'postgres',
+    sqlDialect: process.env.SQL_DIALECT,
+    sqlDatabasePath: process.env.SQL_DATABASE_PATH,
     sqlConnectionUrl: process.env.DATABASE_URL,
   },
 }).then(({port}) => console.log('LHCI listening on port', port));
